83d5de92be3f7101b177af4e98c69e2686c9689f
Niklas Mähler <niklas.mahler@gmail.com>
2025-01-10 14:18:12 +0100

    Add plumber file support to config rm

--- cmd/config/rm.go
+++ cmd/config/rm.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"errors"
 	"log/slog"
 	"os"
 	"path/filepath"
@@ -17,12 +18,17 @@ var rmCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		configDir := viper.GetString("config-home")
 		path := filepath.Join(configDir, args[0])
-		_, err := plumber.ConfigFromPath(path)
+		pf, err := plumber.ReadPlumberFile(path)
 		if err != nil {
-			slog.Error("error initialising config", "config", args[0], "error", err.Error())
+			var pfNotFound plumber.PlumberFileNotFound
+			if errors.As(err, &pfNotFound) {
+				slog.Error("no such config", "path", path, "error", pfNotFound)
+				os.Exit(1)
+			}
+			slog.Error("error reading plumber file", "path", path, "error", err)
 			os.Exit(1)
 		}
-		if err := os.RemoveAll(path); err != nil {
+		if err := os.RemoveAll(pf.Path); err != nil {
 			slog.Error("error removing config", "config", args[0], "error", err.Error())
 			os.Exit(1)
 		}
