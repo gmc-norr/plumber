29cd5d8aedefe13df27552aad43c46c75001aa17
Niklas Mähler <niklas.mahler@gmail.com>
2026-04-30 13:04:13 +0200

    Wording

--- cmd/run.go
+++ cmd/run.go
@@ -235,7 +235,7 @@ func NewRunCmd(v *viper.Viper) *cobra.Command {
 			// Check that the config checksums match
 			checksums, err := plumber.ReadChecksums(filepath.Join(pf.Path, plumber.ChecksumFile))
 			if err != nil {
-				return fmt.Errorf(`failed to read config file checksums, download them again with "plumber config download --force %s %s"`, pf.Pipelines[0].Pipeline.Repo, pf.Pipelines[0].Version)
+				return fmt.Errorf(`failed to read config file checksums, download a fresh config with "plumber config download --force %s %s"`, pf.Pipelines[0].Pipeline.Repo, pf.Pipelines[0].Version)
 			}
 			if err := checksums.Check(pf.Path); err != nil {
 				errs := err.(interface{ Unwrap() []error }).Unwrap()
